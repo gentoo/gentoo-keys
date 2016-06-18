47e382281a6ad2a19080c78c163c8ef681f12b5b
aeroniero33 <justthisthing@gmail.com>
2016-06-18 16:59:58 +0000

    Fixed a bug in the verify function

--- gkeys/gkeys/actions.py
+++ gkeys/gkeys/actions.py
@@ -749,7 +749,6 @@ class Actions(ActionBase):
             messages.append(_unicode("Using config defaults..: %s %s")
                 % (args.category, args.nick))
             return self.verify(args, messages)
-
         return self._verify(args, key, messages)
 
 
@@ -838,6 +837,8 @@ class Actions(ActionBase):
                         break
                     else:
                         sig_path = None
+            elif signature:
+                sig_path = os.path.abspath(signature)
         self.logger.info("Verifying file...")
         verified = False
         results = self.gpg.verify_file(key, sig_path, filepath)

9aae3603e6785ac216e97c867c09980eefedd7b6
Brian Dolbec <dolsen@gentoo.org>
2018-07-05 13:02:29 -0700

    gkeys: Update version and authors
    Signed-off-by: Brian Dolbec <dolsen@gentoo.org>

--- gkeys/gkeys/__init__.py
+++ gkeys/gkeys/__init__.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 from gkeys.action_map import Action_Map, Available_Actions
 
 
-__version__ = '0.2'
+__version__ = '0.3'
 __license__ = 'GPLv2'
 
 if sys.version_info[0] >= 3:
@@ -32,5 +32,5 @@ the GPG keys used for validation on users and Gentoo's infrastracutre servers.
 Gentoo Keys is able to verify GPG keys used for Gentoo's release media,
 such as installation CD's, Live DVD's, packages and other GPG signed documents.''',
     'sub-cmds': subdata,
-    'authors': ['Brian Dolbec <dolsen@gentoo.org>', 'Pavlos Ratis <dastergon@gentoo.org>'],
+    'authors': ['Brian Dolbec <dolsen@gentoo.org>', 'Pavlos Ratis <dastergon@gentoo.org>', 'aeroniero33 <justthisthing@gmail.com>'],
 }

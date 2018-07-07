4dddb4672aed28901ad6de1db5e520d73720a898
Brian Dolbec <dolsen@gentoo.org>
2018-07-07 06:52:50 -0700

    gkeyldap config.py: py3 update xrange ==> range
    Signed-off-by: Brian Dolbec <dolsen@gentoo.org>

--- gkeys-ldap/gkeyldap/config.py
+++ gkeys-ldap/gkeyldap/config.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*-
 
 # Redundancy -- ldap{1,2,3,4}
-default_server = ['ldap://ldap%d.gentoo.org' % i for i in xrange(1,5)]
+default_server = ['ldap://ldap%d.gentoo.org' % i for i in range(1,5)]
 # add uid to the results so you don't have to
 # separate it out of the results tuple[0] value
 default_fields = ['uid', 'cn', 'mail', 'gentooStatus', 'gpgfingerprint']
